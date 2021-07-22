408ff8ba2aeddc472972fd2855130ccd78e4c4e4
Mathias-Hadji <68545712+Mathias-Hadji@users.noreply.github.com>
2021-07-22 18:04:54 +0200

    Update createUserFolders

--- middleware/createUserFolders.js
+++ middleware/createUserFolders.js
@@ -2,6 +2,18 @@ const fs = require('fs');
 
 
 module.exports = (newUser) => {
+    
+    if (!fs.existsSync('./images/users')) {
+        // Create directory ./images
+        fs.mkdir(`./images/users`, function(err) {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log("New directory successfully created.")
+            }
+        }) 
+    }
+    
     // Create directory images/users/user-ID
     fs.mkdir(`./images/users/id-${newUser.id}`, function(err) {
         if (err) {
